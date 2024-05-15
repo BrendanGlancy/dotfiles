aaab7924ce88ab98c9a44231d4c054eea6408eff
brendanglancy <bglance68@gmail.com>
2024-05-14 20:34:58 -0400

    new nvim dotfiles

---
+++ nvim/lua/theprimeagen/lazy/colors.lua
@@ -0,0 +1,48 @@
+function ColorMyPencils(color)
+	color = color or "kanagawa"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+return {
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "transparent", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end
+    },
+
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        config = function()
+            require('kanagawa').setup({
+                disable_background = true,
+            })
+
+            vim.cmd("colorscheme kanagawa")
+
+            ColorMyPencils()
+        end
+    },
+
+
+}

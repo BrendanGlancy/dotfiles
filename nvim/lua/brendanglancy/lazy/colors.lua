7bf089eb872d77bf2d8c848b8145b342ea60b79c
brendanglancy <bglance68@gmail.com>
2024-12-24 15:47:53 -0500

    new colors who this

--- nvim/lua/brendanglancy/lazy/colors.lua
+++ nvim/lua/brendanglancy/lazy/colors.lua
@@ -4,19 +4,50 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- Set line number colors
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#89ddff" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "none" })
+
+    -- Disable sign column
+    vim.wo.signcolumn = "no"
 end
 
 return {
-
     {
-        "erikbackman/brightburn.vim",
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end
     },
 
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
         config = function()
+            require('kanagawa').setup({
+                commentStyle = { italic = false },
+                keywordStyle = { italic = false },
+                statementStyle = { bold = false },
+                theme = "wave", -- Load "wave" theme when 'background' option is not set
+            })
+
+            vim.cmd("colorscheme kanagawa")
+
             ColorMyPencils()
         end
     },
@@ -46,32 +77,10 @@ return {
                 palette_overrides = {},
                 overrides = {},
                 dim_inactive = false,
-                transparent_mode = false,
+                transparent_mode = true,
             })
         end,
     },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
-
     {
         "rose-pine/neovim",
         name = "rose-pine",
@@ -80,12 +89,11 @@ return {
                 disable_background = true,
                 styles = {
                     italic = false,
+                    transparency = true;
                 },
             })
 
             ColorMyPencils();
         end
     },
-
-
 }

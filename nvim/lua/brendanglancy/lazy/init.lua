af72147a3dd90858052152aa86b9ee7b6d1386aa
BrendanGlancy <bglance68@gmail.com>
2025-01-21 15:39:14 -0500

    force update from my mac

--- nvim/lua/brendanglancy/lazy/init.lua
+++ nvim/lua/brendanglancy/lazy/init.lua
@@ -4,6 +4,16 @@ return {
         name = "plenary"
     },
 
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
     -- "github/copilot.vim",
     "eandrju/cellular-automaton.nvim",
     "luarocks/luarocks",

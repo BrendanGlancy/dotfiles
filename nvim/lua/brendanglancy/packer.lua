525ee86b282a618de9307921c50a69a37d6cf4a5
brendanglancy <bglance68@gmail.com>
2025-04-11 20:13:23 -0400

    stash

--- nvim/lua/brendanglancy/packer.lua
+++ nvim/lua/brendanglancy/packer.lua
@@ -75,9 +75,7 @@ return require("packer").startup(function(use)
   }
 
   use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
 
 end)
-

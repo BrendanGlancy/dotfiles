27590e6b509ba65178a3ffad19a07c9af8b85892
Brendan Glancy <bglance68@gmail.com>
2025-10-28 09:43:09 -0400

    neotree

--- nvim/lua/brendanglancy/lazy/telescope.lua
+++ nvim/lua/brendanglancy/lazy/telescope.lua
@@ -1,47 +1,49 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+	tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
-    config = function()
-        require('telescope').setup({
-            defaults = {
-                file_ignore_patterns = {
-                    "node_modules/",
-                    "%.lock",
-                    "%.env",
-                    "build/",
-                    "dist/",
-                    "go%.mod",
-                    "go%.sum",
-                    "*.test.js",
-                    "__tests__",
-                },
-            }
-        })
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules/",
+					"%.lock",
+					"%.env",
+					"build/",
+					"dist/",
+					"go%.mod",
+					"go%.sum",
+					"*.test.js",
+					"__tests__",
+				},
+        path_displays = { "smart" },
+        sorting_strategy = "ascending",
+			},
+		})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fw', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>fs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fz', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.current_buffer_fuzzy_find({ search = word })
-        end)
-    end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>fw", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>pWs", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>fs", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fz", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.current_buffer_fuzzy_find({ search = word })
+		end)
+	end,
 }

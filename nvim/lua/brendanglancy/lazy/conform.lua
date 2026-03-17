b0bd692459c88c81126936708fe42c63aee0127c
Brendan Glancy <bglance68@gmail.com>
2026-03-17 10:20:12 -0400

    new config

--- nvim/lua/brendanglancy/lazy/conform.lua
+++ nvim/lua/brendanglancy/lazy/conform.lua
@@ -3,8 +3,19 @@ return {
 	opts = {},
 	config = function()
 		require("conform").setup({
+			log_level = vim.log.levels.DEBUG,
+			notify_on_error = true,
 			formatters_by_ft = {
-				lua = { "prettier" },
+				lua = { "stylua" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				json = { "prettier" },
+			},
+			format_on_save = {
+				timeout_ms = 3000,
+				lsp_fallback = false,
 			},
 		})
 	end,
--- nvim/lua/brendanglancy/lazy/lsp.lua
+++ nvim/lua/brendanglancy/lazy/lsp.lua
@@ -17,11 +17,6 @@ return {
 	},
 
 	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				"prettier",
-			},
-		})
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
